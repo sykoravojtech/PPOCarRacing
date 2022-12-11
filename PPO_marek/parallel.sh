049498f6c537130b95acfe1dbeab68ca80c10b5d
Vojtech Sykora <vojtasykor@gmail.com>
2022-12-11 17:19:02 +0100

    924 score

--- PPO_marek/parallel.sh
+++ PPO_marek/parallel.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task 1
 #SBATCH --mem-per-cpu 32G
 #SBATCH --job-name car
-#SBATCH --output slurm-entropy_coeff-%j.out   ## filename of the output; the %j is equal to jobID; default is slurm-[jobID].out
+#SBATCH --output slurm-lr-constant-%j.out   ## filename of the output; the %j is equal to jobID; default is slurm-[jobID].out
 
 
 srun --ntasks=8 -l --multi-prog ./commands.conf
